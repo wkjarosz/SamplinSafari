3b4a3ba2e2ddeeadd6a8d01849aa507e8b035ae2
Wojciech Jarosz <wkjarosz@users.noreply.github.com>
2023-12-17 15:05:06 +1100

    adding portable bitcounts

--- src/sampler/GrayCode.cpp
+++ src/sampler/GrayCode.cpp
@@ -2,6 +2,7 @@
     \author Wojciech Jarosz
 */
 
+#include <bitcount.h>
 #include <sampler/GrayCode.h>
 
 GrayCode::GrayCode(unsigned n)
@@ -21,21 +22,23 @@ void GrayCode::regenerate()
         m_samples[V].x = m_samples[V * n].y = res * (V * n); // Sample in first stratum of each row and column
 
     uint32_t u = 0;
+    // Iterate through strata up the column
     for (unsigned U = 1; U < n; U++)
-    {                                             // Iterate through strata up the column
-        uint32_t newBitsCount = __builtin_ctz(U); // This number of bits have to be replaced on the left
-        uint32_t reservedBitsCount =
-            log2n - 1 - newBitsCount; // Number of reserved bits from previous sample. 1 is the octave toggling bit.
+    {
+        // This number of bits have to be replaced on the left
+        uint32_t newBitsCount = bit_ctz(U);
+        // Number of reserved bits from previous sample. 1 is the octave toggling bit.
+        uint32_t reservedBitsCount = log2n - 1 - newBitsCount;
 
         // Generate a random xor mask for new bits, leading to Faure-Tezuka scrambling.
         uint32_t randomBits = m_randomize ? (m_rand.nextUInt() & ((1 << newBitsCount) - 1)) >> 1 : 0;
 
+        // Apply xor mask: random bits, a single 1, and zeros for reserved bits
         // This generates a Gray code ordering of van der Corput sequences.
-        u ^= ((randomBits << 1) | 1)
-             << reservedBitsCount; // Apply xor mask: random bits, a single 1, and zeros for reserved bits
+        u ^= ((randomBits << 1) | 1) << reservedBitsCount;
 
+        // Iterate through columns
         for (uint32_t V = 0; V < n; V++)
-            // Iterate through columns
             m_samples[U * n + V].x = m_samples[V * n + U].y = res * (V * n + u);
     }
 }
